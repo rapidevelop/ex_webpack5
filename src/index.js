81d7894d2d4502c417b6858e8e83ef3b32966215
rapidevelop <i.vafadarnezhad@gmail.com>
2020-12-19 06:41:30 +0330

    feat: install webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const $ = require('jquery');
+
+$('body').css({ 'background-color': '#eeeeee' });
+$('h1#title')
+  .css({ 'font-size': '80px', 'text-align': 'center', 'margin-top': '100px' })
+  .text('Hello Webpack!!!');
