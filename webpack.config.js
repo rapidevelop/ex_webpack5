5aede42c6d498385eb0f45ecfce78e126e18a0ab
rapidevelop <i.vafadarnezhad@gmail.com>
2020-12-24 22:49:09 +0330

    build(webpack): install html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -12,4 +13,11 @@ module.exports = {
     port: 3000,
     open: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
 };
